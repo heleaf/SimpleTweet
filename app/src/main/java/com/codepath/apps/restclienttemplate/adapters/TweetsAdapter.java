8a93ef06a1010998956959d71cb6d1764f75de96
Helena Yang <hfy@fb.com>
2022-06-08 14:22:34 -0700

    added relative timestamp

--- app/src/main/java/com/codepath/apps/restclienttemplate/adapters/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/adapters/TweetsAdapter.java
@@ -2,6 +2,7 @@ package com.codepath.apps.restclienttemplate.adapters;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,7 +18,10 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.codepath.apps.restclienttemplate.R;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder> {
 
@@ -53,6 +57,45 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         return tweets.size();
     }
 
+    // taken from: https://gist.github.com/nesquena/f786232f5ef72f6e10a7
+    private static final int SECOND_MILLIS = 1000;
+    private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+    private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+    private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
+
+    public String getRelativeTimeAgo(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        try {
+            long time = sf.parse(rawJsonDate).getTime();
+            long now = System.currentTimeMillis();
+
+            final long diff = now - time;
+            if (diff < MINUTE_MILLIS) {
+                return "just now";
+            } else if (diff < 2 * MINUTE_MILLIS) {
+                return "a minute ago";
+            } else if (diff < 50 * MINUTE_MILLIS) {
+                return diff / MINUTE_MILLIS + "m";
+            } else if (diff < 90 * MINUTE_MILLIS) {
+                return "an hour ago";
+            } else if (diff < 24 * HOUR_MILLIS) {
+                return diff / HOUR_MILLIS + "h";
+            } else if (diff < 48 * HOUR_MILLIS) {
+                return "yesterday";
+            } else {
+                return diff / DAY_MILLIS + "d";
+            }
+        } catch (ParseException e) {
+            Log.i("TweetsAdapter", "getRelativeTimeAgo failed");
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
     // define a viewholder
     public class ViewHolder extends RecyclerView.ViewHolder {
         final int MEDIA_HEIGHT = (Resources.getSystem().getDisplayMetrics().heightPixels / 4);
@@ -61,6 +104,7 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         TextView tvBody;
         TextView tvScreenName;
         ImageView tvMedia;
+        TextView relativeTimeStamp;
         public ViewHolder(@NonNull View itemView){ // one tweet
             super(itemView);
             ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
@@ -70,11 +114,13 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
             ViewGroup.LayoutParams tvMediaParams = tvMedia.getLayoutParams();
             tvMediaParams.height = MEDIA_HEIGHT;
             tvMedia.setLayoutParams(tvMediaParams);
+            relativeTimeStamp = itemView.findViewById(R.id.relativeTimeStamp);
         }
 
         public void bind(Tweet tweet) {
             tvBody.setText(tweet.body);
             tvScreenName.setText(tweet.user.screenName);
+            relativeTimeStamp.setText(getRelativeTimeAgo(tweet.createdAt));
             Glide.with(context).load(tweet.user.profileImageUrl)
                     .transform(new RoundedCorners(CORNER_RADIUS))
                     .into(ivProfileImage);
